4d2c5990b8b1294cdd94afa1a55a89869957a894
Seth Hendrick <seth@shendrick.net>
2017-02-26 20:54:29 -0500

    Added licensing to RSS feed generator.

--- _rssfeed/RssFeedGenerator/RssFeedReader.cs
+++ _rssfeed/RssFeedGenerator/RssFeedReader.cs
@@ -1,4 +1,9 @@
-﻿using System;
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
