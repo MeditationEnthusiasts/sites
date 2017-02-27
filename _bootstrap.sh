a28c004e4d3dc4144bf359d4555fec0d5abe8bb7
Seth Hendrick <seth@shendrick.net>
2017-02-26 20:41:54 -0500

    Added a bootstrap script that will put the correct system dlls in the correct spot.

---
+++ _bootstrap.sh
@@ -0,0 +1,3 @@
+# Use this to add System Plugins that Pretzel is not aware about into the _plugins folder.
+# This assumes a Linux System.  Use _bootstrap.bat for Windows..
+ln -s /usr/lib/mono/4.5-api/System.ServiceModel.dll ./_plugins/System.ServiceModel.dll
